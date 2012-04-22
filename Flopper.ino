31f8c76e58f5a56416aac4387137234498ebeb4c
Thorsten Kummerow <thorsten.kummerow@googlemail.com>
2012-04-22 20:51:08 +0200

    Implemented needed functions. Now testing.

--- Flopper.ino
+++ Flopper.ino
@@ -6,9 +6,10 @@
 #define TIMER_RESOLUTION  40    // microseconds
 #define MIDI_NOTES        128   // Number of existing midi notes
 #define POSITION_MAX      158   // tracks x 2 (each track is two stepper motor steps wide)
-#define DRIVES            20    // Number of connected drives
+#define DRIVES            16    // Number of connected drives
 #define DIR_UP            0     // Upward direction
 #define DIR_DOWN          1     // Downward direction
+#define RANDOM_NUMBERS    30    // Size of random drive number table
 
 /**
  * @brief  Note length in microseconds
@@ -34,9 +35,9 @@ const int noteLength[MIDI_NOTES] = {
 int noteTicks[MIDI_NOTES];
 
 /**
- * Note periods in ticks of the playing drives.
+ * Note of the playing drives.
  */
-int currentPeriod[DRIVES];
+byte currentNote[DRIVES];
 
 /**
  * Elapsed ticks of playing drives.
@@ -52,6 +53,26 @@ byte currentDir[DRIVES];
  * Current head position.
  */
 int currentPos[DRIVES];
+
+const byte uRegisters = (((DRIVES * 2) - 1) / 8) + 1;
+
+/**
+ * Shift register state.
+ */
+byte registerState[uRegisters];
+
+struct DrivePinInfo {
+  byte uReg;      // 0 - based register index the drive is connected to
+  byte uDirPin;   // number of direction pin
+  byte uStepPin;  // number of motor pin
+};
+
+DrivePinInfo drivePinInf[DRIVES];
+
+/**
+ * Next drive. Needed to cycle through all connected drives when playing polyphonically.
+ */
+byte uNextDrive = 0;
 
 void setup()
 {
@@ -69,7 +90,7 @@ void setup()
 
   // Init arrays
   for (i = 0; i < DRIVES; ++i) {
-    currentPeriod[i] = 0;
+    currentNote[i] = 0;
     currentTicks[i] = 0;
     
     // Init drive position to max and direction to down so
@@ -77,6 +98,10 @@ void setup()
     currentDir[i] = DIR_DOWN;
     currentPos[i] = POSITION_MAX;
   }
+  
+  for (i = 0; i < uRegisters; ++i) registerState[i] = 0;
+
+  initDrivePinInf();
 
   // Register MIDI callback functions
   usbMIDI.setHandleNoteOff(_NoteOff);
@@ -85,7 +110,7 @@ void setup()
   usbMIDI.setHandleControlChange(_ControlChange);
   usbMIDI.setHandleProgramChange(_ProgramChange);
   usbMIDI.setHandleAfterTouch(AfterTouch);
-  usbMIDI.setHandlePitchChange(PitchChange);
+  usbMIDI.setHandlePitchChange(PitchChange);  
   
   // Init drive positions
   resetAllPositions();
@@ -101,26 +126,66 @@ void loop()
   usbMIDI.read();
 } // loop
 
-inline void tick(int drive) 
+void initDrivePinInf()
 {
+  for (int i = 0; i < DRIVES; ++i) {
+    drivePinInf[i].uReg = ((i * 2) - 1) / 8;
+    drivePinInf[i].uDirPin = i % 8;
+    drivePinInf[i].uStepPin = drivePinInf[i].uDirPin + 1;
+  }
+} // initDrivePinInf
+
+/**
+ * Sets register state so drive nDrive makes one step on updateRegisters().
+ */
+inline void prepareStep(byte uDrive)
+{ 
+  byte uReg, uDirPin, uStepPin, uStepPinState;
   
+  if (!currentPos[uDrive]) currentDir[uDrive] = DIR_UP;
+  else if (currentPos[uDrive] == POSITION_MAX) currentDir[uDrive] = DIR_DOWN;
+
+  uReg = drivePinInf[uDrive].uReg;
+  uStepPin = drivePinInf[uDrive].uStepPin;
+  uDirPin = drivePinInf[uDrive].uDirPin;
+
+  // Get current motor pin state
+  uStepPinState = registerState[uReg] & (1 << uStepPin);
+
+  // Clear direction pin state
+  registerState[uReg] &= ~(1 << uDirPin);
+  
+  // Set new pin states
+  registerState[uReg] ^= uStepPinState;                    // Toggle motor pin
+  registerState[uReg] |= (currentDir[uDrive] << uDirPin);  // Set direction pin
+  
+  if (DIR_UP) ++currentPos[uDrive]; else --currentPos[uDrive];
+} // prepareStep
+
+inline void tick(byte uDrive) 
+{
+  if (!currentNote[uDrive]) return;
+  ++currentTicks[uDrive];
+  if (currentTicks[uDrive] != noteTicks[currentNote[uDrive]]) return;
+  prepareStep(uDrive);
+  currentTicks[uDrive] = 0;
 } // tick
 
 void tick()
 {
-  int i;
-  
-  for (i = 0; i < DRIVES; ++i) tick(i);
+  for (int i = 0; i < DRIVES; ++i) tick(i);  
+  updateRegisters();
 } // tick
 
 void _NoteOn(byte channel, byte note, byte velocity)
 {
-  // ToDo: implement.
+  if (velocity) playNote(note);
+  else stopNote(note);
 } // NoteOn
 
 void _NoteOff(byte channel, byte note, byte velocity)
 {
-  // ToDo: implement.
+  stopNote(note);
 } // NoteOff
 
 void VelocityChange(byte channel, byte note, byte velocity)
@@ -148,11 +213,53 @@ void PitchChange(uint8_t channel, uint16_t pitch)
   // ToDo: implement.
 } // PitchChange
 
+void updateRegisters()
+{
+  digitalWrite(SS, LOW);
+  for (byte i = 0; i < uRegisters; ++i) SPI.transfer(registerState[i]);
+  digitalWrite(SS, HIGH);
+} // updateRegisters
 
+/**
+ * Advances stepper motor positions of all drives until
+ * Every drive reaches position 0.
+ */
 void resetAllPositions()
 {
-  // ToDo: implement
+  for (int i = 0; i < POSITION_MAX * 2; ++i) {
+    for (int j = 0; j < DRIVES; ++j) {
+      if (currentPos[i]) prepareStep(i);
+    }
+    updateRegisters();
+  }
 } // resetAllPositions
 
+byte findNextIdleDrive()
+{ 
+  byte uResult = uNextDrive;    // Return uNextDrive if polyphony is used up
+  for (int i = 0; i < DRIVES; ++i) {
+    if (!currentNote[uNextDrive]) {
+      uResult = uNextDrive++;
+      if (uNextDrive == DRIVES) uNextDrive = 0;
+      break;
+    }
+  }
+  return uResult;
+} // findFirstIdleDrive
+
+inline int findDrivePlayingNote(byte uNote)
+{
+  for (int i = 0; i < DRIVES; ++i) if (currentNote[i] == uNote) return i;
+} // findDrivePlayingNote
+
+void playNote(byte uNote)
+{  
+  currentNote[findNextIdleDrive()] = uNote;
+} // playNote
+
+void stopNote(byte uNote)
+{
+  currentNote[findDrivePlayingNote(uNote)] = 0;
+} // stopNote
 
 
